3f5fcbedaaafba2a381a04a386d48b1ac967f0b3
shun4midx <shun4midx@gmail.com>
2025-05-17 18:53:48 +0800

    Even more hardcode no hover

---
+++ training-game/main-scripts/disable-hover.js
@@ -0,0 +1,10 @@
+/********************************************
+* Copyright (c) 2025 Shun/翔海 (@shun4midx) *
+* Project: School Project of Training Game *
+* File Type: JS file for website           *
+* File: disable-hover.js                   *
+********************************************/
+
+if (window.matchMedia('(hover: none)').matches) {
+  document.documentElement.classList.add('no-hover');
+}
